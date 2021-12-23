7a7bda2a5b96704dfb7157ec4aac6d50a472d666
jcpena-sbd <jcpena@solutionspr.net>
2021-12-22 22:35:30 -0500

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,6 +8,7 @@ import { TileLayoutResizeEvent } from '@progress/kendo-angular-layout';
                 <kendo-tilelayout-item-body>
                 <map-views-chart></map-views-chart>
                     <section class="content" *ngIf="html" [innerHTML]="html"></section>
+                    <div id="mapView"></div>
                 </kendo-tilelayout-item-body>
             </kendo-tilelayout-item>
 
@@ -57,102 +58,94 @@ import { TileLayoutResizeEvent } from '@progress/kendo-angular-layout';
             </kendo-tilelayout-item>
         </kendo-tilelayout>
         <script>
-        var regions = [
-            {
-                "region_name": "Pueblo de Camuy",
-                "region_code": "pdc",
-                "cases": 3
-            },{
-                "region_name": "Puente",
-                "region_code": "pnt",
-                "cases": 10
-            },{
-                "region_name": "Membrillo",
-                "region_code": "mem",
-                "cases": 20
-            },{
-                "region_name": "Yaguada",
-                "region_code": "ygd",
-                "cases": 50
-            },{
-                "region_name": "Camuy Arriba",
-                "region_code": "car",
-                "cases": 80
-            },{
-                "region_name": "Ciénagas",
-                "region_code": "cng",
-                "cases": 3
-            },{
-                "region_name": "Zanja",
-                "region_code": "zan",
-                "cases": 13
-            },{
-                "region_name": "Abra Honda",
-                "region_code": "abh",
-                "cases": 3
-            },{
-                "region_name": "Piedra Gorda",
-                "region_code": "pdg",
-                "cases": 0
-            },{
-                "region_name": "Puerton",
-                "region_code": "prt",
-                "cases": 30
-            },{
-                "region_name": "Cibao",
-                "region_code": "cib",
-                "cases": 13
-            },{
-                "region_name": "Quebrada",
-                "region_code": "qub",
-                "cases": 18
-            },{
-                "region_name": "Santiago",
-                "region_code": "stg",
-                "cases": 3
-            }
-        
-        ];
-       
-        var temp_array= regions.map(function(item){
-            return item.cases;
+  var regions = [
+  {
+        "region_name": "Pueblo de Camuy",
+        "region_code": "pdc",
+        "cases": 3
+    },{
+        "region_name": "Puente",
+        "region_code": "pnt",
+        "cases": 10
+    },{
+        "region_name": "Membrillo",
+        "region_code": "mem",
+        "cases": 20
+    },{
+        "region_name": "Yaguada",
+        "region_code": "ygd",
+        "cases": 50
+    },{
+        "region_name": "Camuy Arriba",
+        "region_code": "car",
+        "cases": 80
+    },{
+        "region_name": "Ciénagas",
+        "region_code": "cng",
+        "cases": 3
+    },{
+        "region_name": "Zanja",
+        "region_code": "zan",
+        "cases": 13
+    },{
+        "region_name": "Abra Honda",
+        "region_code": "abh",
+        "cases": 3
+    },{
+        "region_name": "Piedra Gorda",
+        "region_code": "pdg",
+        "cases": 0
+    },{
+        "region_name": "Puerton",
+        "region_code": "prt",
+        "cases": 30
+    },{
+        "region_name": "Cibao",
+        "region_code": "cib",
+        "cases": 13
+    },{
+        "region_name": "Quebrada",
+        "region_code": "qub",
+        "cases": 18
+    },{
+        "region_name": "Santiago",
+        "region_code": "stg",
+        "cases": 3
+    }
+  ];
+  var temp_array= regions.map(function(item){
+    return item.cases;
+  });
+  var highest_value = Math.max.apply(Math, temp_array);
+  $(function() {
+    for(i = 0; i < regions.length; i++) {
+        $('#'+ regions[i].region_code)
+        .css({'fill': 'rgba(11, 104, 170,' + regions[i].cases/highest_value +')'})
+        .data('region', regions[i]);
+    }
+    $('.map .cls-1').mouseover(function (e) {
+        var region_data=$(this).data('region');
+        $('<div class="info_panel">'+
+            region_data.region_name + '<br>' +
+            'Cases: ' + region_data.cases.toLocaleString("en-UK") +
+            '</div>'
+         )
+        .appendTo('body');
+    })
+    .mouseleave(function () {
+        $('.info_panel').remove();
+    })
+    .mousemove(function(e) {
+        var mouseX = e.pageX, //X coordinates of mouse
+            mouseY = e.pageY; //Y coordinates of mouse
+        $('.info_panel').css({
+            top: mouseY-50,
+            left: mouseX - ($('.info_panel').width()/2)
         });
-        var highest_value = Math.max.apply(Math, temp_array);
-        
-        $(function() {
-        
-            for(i = 0; i < regions.length; i++) {
-        
-                $('#'+ regions[i].region_code)
-                .css({'fill': 'rgba(11, 104, 170,' + regions[i].cases/highest_value +')'})
-                .data('region', regions[i]);
-            }
-            
-             
-            $('.map .cls-1').mouseover(function (e) {
-                var region_data=$(this).data('region');
-                $('<div class="info_panel">'+
-                    region_data.region_name + '<br>' +
-                    'Cases: ' + region_data.cases.toLocaleString("en-UK") +
-                    '</div>'
-                 )
-                .appendTo('body');
-            })
-            .mouseleave(function () {
-                $('.info_panel').remove();
-            })
-            .mousemove(function(e) {
-                var mouseX = e.pageX, //X coordinates of mouse
-                    mouseY = e.pageY; //Y coordinates of mouse
-        
-                $('.info_panel').css({
-                    top: mouseY-50,
-                    left: mouseX - ($('.info_panel').width()/2)
-                });
-            });
-        
-        });       
-        </script>
+    });
+    $('.map').appendTo('mapView');
+  });
+</script>
     `,
 })
 export class AppComponent {}
