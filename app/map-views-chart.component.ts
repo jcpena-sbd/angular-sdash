ab83c9e4e77af361714aded5ff84db1b5058d298
jcpena-sbd <jcpena@solutionspr.net>
2021-12-22 22:26:26 -0500

    2 files modified

--- app/map-views-chart.component.ts
+++ app/map-views-chart.component.ts
@@ -3,15 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'map-views-chart',
   templateUrl: 'map-views.chart.component.html',
-  
 })
-export class MapViewsChartComponent  implements OnInit {
+export class MapViewsChartComponent implements OnInit {
   html: string;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.html = `<h2>We're currently undergoing essential maintenance, we'll be back online shortly.</h2>
-    <p>If you have any questions please contact our customer services team via email at <a href="mailto:support@domain.com?subject=Support Requested">support@domain.com</a></p>`;
+    this.html = `<div id="mapView"></div>`;
   }
 }
